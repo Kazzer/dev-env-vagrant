7a7fb4accc8ea97c2622ab8d2f7a2630a1eb2281
Kadeem Hassam <kadeem@gmail.com>
2015-09-28 15:17:43 -0700

    [Fixed] group template to check for exact name instead of prefix

--- templates/group_template.sh
+++ templates/group_template.sh
@@ -4,7 +4,7 @@ source '/tmp/vagrant/common.sh'
 # group name
 group="example"
 
-if [ -z "$(grep -e "^${group}" /etc/group)" ]
+if [ -z "$(grep -e "^${group}:" /etc/group)" ]
 then
     log_info "Configuring group ${group}"
     sudo /usr/sbin/groupadd "${group}"
